6ff7c47492ed13865934ce425c3efdde7bf97e89
arifulsajib <allaboutsajib@gmail.com>
2021-10-27 18:26:42 +0600

    change

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("services");
+  res.send("Genius car mechanic server");
 });
 
 app.listen(port, () => {
